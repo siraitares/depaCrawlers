0170a833dcce76cf2920326a7c263ee988ad56dc
Pablo Carranza <pablo_carranza@hotmail.com>
2017-08-25 01:01:32 -0700

    Cleaned code

--- depasCL.py
+++ depasCL.py
@@ -2,12 +2,15 @@
 
 import urllib2
 from bs4 import BeautifulSoup
-import time
 
 baseUrl = "https://tijuana.craigslist.com.mx"
 url = "https://tijuana.craigslist.com.mx/search/apa"
 pagesToCrawl = 5
 counter = 1
+minPesos = 1500
+maxPesos = 6000
+minDls = 50
+maxDls = 350
 f = open('listadoCL.csv', 'w')
 f.write("Título,Precio,Link,Fecha de publicación\n")
 
@@ -17,23 +20,16 @@ while (counter <= pagesToCrawl):
     listings = soup.find_all('p', attrs={'class': 'result-info'})
     nextButton = soup.find('a', attrs={'class': 'next'})
     url = baseUrl + nextButton['href']
-    print 'iteration: ' + str(counter)
-
     for i in listings:
         price = i.find('span', attrs={'class': 'result-price'})
         if price:
             price = i.find('span', attrs={'class': 'result-price'}).text.encode('utf-8')
             price = int(price.replace('$', ''))
-            if ((price >= 50) and (price <= 350)) or ((price >= 1500) and (price <= 6000)):
+            if ((minDls <= price <= maxDls) or (minPesos <= price <= maxPesos)):
                 title = i.find('a').text.encode('utf-8')
                 title = title.replace(',', '\'')
                 date = i.find('time', attrs={'class': 'result-date'}).text.encode('utf-8')
                 link = i.find('a')['href'].encode('utf-8')
                 f.write(title + ',' + str(price) + ',' + baseUrl + link + ',' + date + '\n')
-                print title + ',' + str(price) + ',' + baseUrl + link + ',' + date
-            else:
-            	print "Fuera del if " + str(price)
-        else:
-        	print "No trae valor"
     counter += 1
 f.close()
--- depasVA.py
+++ depasVA.py
@@ -2,12 +2,15 @@
 
 import urllib2
 from bs4 import BeautifulSoup
-import time
 
 baseUrl = "https://www.vivanuncios.com.mx"
 url = "https://www.vivanuncios.com.mx/s-renta-inmuebles/tijuana/v1c1098l10015p1"
 pagesToCrawl = 10
 counter = 1
+minPesos = 1500
+maxPesos = 6000
+minDls = 50
+maxDls = 350
 f = open('listadoVA.csv', 'w')
 f.write("Título,Precio,Link,Fecha de publicación\n")
 
@@ -17,24 +20,17 @@ while (counter <= pagesToCrawl):
     listings = soup.find_all('div', attrs={'class': 'result-link'})
     nextButton = soup.find('a', attrs={'class': 'next'})
     url = baseUrl + nextButton['href']
-    print 'iteration: ' + str(counter)
-
     for i in listings:
         price = i.find('span', attrs={'class': 'amount'})
         if price:
             price = i.find('span', attrs={'class': 'amount'}).text.encode('utf-8')
             price = int(price.replace('$', '').replace(',', ''))
-            if ((price >= 50) and (price <= 350)) or ((price >= 1500) and (price <= 6000)):
+            if ((minDls <= price <= maxDls) or (minPesos <= price <= maxPesos)):
                 title = i.find('a').text.encode('utf-8')
                 title = title.replace(',', '\'')
                 date = i.find('div', attrs={'class': 'creation-date'}).text.encode('utf-8')
                 date = date.replace('\n', '')
                 link = i.find('a')['href'].encode('utf-8')
                 f.write(title + ',' + str(price) + ',' + baseUrl + link + ',' + date + '\n')
-                print title + ',' + str(price) + ',' + baseUrl + link + ',' + date
-            else:
-            	print "Fuera del if " + str(price)
-        else:
-        	print "No trae valor"
     counter += 1
 f.close()
